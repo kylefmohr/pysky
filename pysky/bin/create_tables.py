b66b3b6010d0bc21f8f936b9f59b35f38a1d5010
Todd Federman <tfederman@hotmail.com>
2025-02-21 18:27:56 -0500

    updates

---
+++ pysky/bin/create_tables.py
@@ -0,0 +1,37 @@
+import sys
+import inspect
+from datetime import datetime
+
+from peewee import Model
+
+from pysky.database import db
+import pysky.models
+
+
+def get_model_classes():
+    class_members = inspect.getmembers(sys.modules["pysky.models"], inspect.isclass)
+    return [
+        (n, cls) for n, cls in class_members if cls.__base__ == pysky.models.BaseModel
+    ]
+
+
+def create_non_existing_tables(db):
+
+    all_model_classes = get_model_classes()
+    missing_table_model_classes = [
+        (n, cls) for n, cls in all_model_classes if not cls.table_exists()
+    ]
+
+    if not missing_table_model_classes:
+        print("All tables already exist.")
+    else:
+        print(
+            f"Creating missing tables: {', '.join(str(cls._meta.table_name) for n,cls in missing_table_model_classes)}"
+        )
+        db.create_tables([cls for n, cls in missing_table_model_classes])
+
+
+if __name__=="__main__":
+
+    # create missing tables:
+    create_non_existing_tables(db)
--- pysky/database.py
+++ pysky/database.py
@@ -1,3 +1,37 @@
-from playhouse.postgres_ext import PostgresqlExtDatabase
+import os
 
-db = PostgresqlExtDatabase('stroma', user='postgres', host='localhost')
+from playhouse.postgres_ext import PostgresqlExtDatabase
+from peewee import SqliteDatabase
+
+
+def get_db_postgresql():
+
+    try:
+        required_pgsql_env_vars = [
+            ("PGDATABASE", "database"),
+            ("PGUSER", "user"),
+            ("PGHOST", "host"),
+            ("PGPASSWORD", "password"),
+        ]
+        optional_pgsql_env_vars = [("PGPORT", "port")]
+        pgsql_args = {
+            argname: os.environ[varname] for varname, argname in required_pgsql_env_vars
+        }
+        pgsql_args.update(
+            {
+                argname: os.getenv(varname)
+                for varname, argname in optional_pgsql_env_vars
+                if os.getenv(varname)
+            }
+        )
+        return PostgresqlExtDatabase(**pgsql_args)
+    except KeyError:
+        return None
+
+
+def get_db_sqlite():
+    sqlite_filename = os.getenv("PYSKY_SQLITE_FILENAME", "pysky.db")
+    return SqliteDatabase(sqlite_filename)
+
+
+db = get_db_postgresql() or get_db_sqlite()
