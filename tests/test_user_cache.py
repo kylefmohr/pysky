f070b3b66991d2e0b0b6a327cf686f068c436ff3
Todd Federman <tfederman@hotmail.com>
2025-03-03 10:44:44 -0500

    different user profile lookups

--- tests/test_user_cache.py
+++ tests/test_user_cache.py
@@ -5,12 +5,12 @@ def test_user_profile_cached_lookup(bsky_no_auth):
 
     current_remote_call_count = bsky_no_auth.remote_call_count
 
-    bsky_no_auth.get_user_profile(actor="did:plc:zcmchxw2gxlbincrchpdjopq")
-    bsky_no_auth.get_user_profile(actor="did:plc:zcmchxw2gxlbincrchpdjopq")
+    profile = bsky_no_auth.get_user_profile(actor="did:plc:zcmchxw2gxlbincrchpdjopq")
+    profile = bsky_no_auth.get_user_profile(actor=profile.handle)
 
     # check that only one request was made because the second one was a cache lookup
     assert bsky_no_auth.remote_call_count == current_remote_call_count + 1
 
     # check the override code
-    bsky_no_auth.get_user_profile(actor="did:plc:zcmchxw2gxlbincrchpdjopq", force_remote_call=True)
+    bsky_no_auth.get_user_profile(actor=profile.did, force_remote_call=True)
     assert bsky_no_auth.remote_call_count == current_remote_call_count + 2
