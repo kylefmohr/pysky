1b2c4853be495790cd395a9a1aaa3c7b47304d7f
Todd Federman <tfederman@hotmail.com>
2025-03-03 15:32:20 -0500

    change test to use non-auth

--- tests/test_bad_requests.py
+++ tests/test_bad_requests.py
@@ -1,37 +1,37 @@
 import pytest
 
-from tests.fixtures import bsky
+from tests.fixtures import bsky_no_auth
 
 
-def test_endpoint_404_failure(bsky):
+def test_endpoint_404_failure(bsky_no_auth):
 
     import pysky
 
     with pytest.raises(pysky.APIError) as e:
         # missing xrpc/ prefix
-        profile = bsky.get(endpoint="app.bsky.actor.getProfile")
+        profile = bsky_no_auth.get(endpoint="app.bsky.actor.getProfile")
 
     assert e.value.apilog.http_status_code == 404
 
 
-def test_endpoint_404_failure_2(bsky):
+def test_endpoint_404_failure_2(bsky_no_auth):
 
     import pysky
 
     with pytest.raises(pysky.APIError) as e:
         # endpoint not available on public host
-        prefs = bsky.get(endpoint="xrpc/app.bsky.actor.getPreferences")
+        prefs = bsky_no_auth.get(endpoint="xrpc/app.bsky.actor.getPreferences")
 
     assert e.value.apilog.http_status_code == 404
 
 
-def test_error_apilog(bsky):
+def test_error_apilog(bsky_no_auth):
 
     import pysky
     from pysky.models import APICallLog
 
     with pytest.raises(pysky.APIError) as e:
-        profile = bsky.get(endpoint="xxx")
+        profile = bsky_no_auth.get(endpoint="xxx")
 
     assert e.value.apilog.http_status_code == 404
 
