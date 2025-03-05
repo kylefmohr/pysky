f6fa182a4e124f48a6b1cfc4c03c59f29bc69ed6
Todd Federman <tfederman@hotmail.com>
2025-03-05 16:18:52 -0500

    add aspect ratio to image upload respnose

--- pysky/helpers.py
+++ pysky/helpers.py
@@ -25,7 +25,8 @@ def get_image_embed(blob_uploads, alt_texts):
 
 
 def get_image(blob_upload, alt_text=None):
-    return {
+
+    image = {
         "alt": alt_text,
         "image": {
             "$type": "blob",
@@ -36,3 +37,9 @@ def get_image(blob_upload, alt_text=None):
             "size": blob_upload.blob.size,
         },
     }
+
+    aspect_ratio = getattr(blob_upload, "aspect_ratio", None)
+    if isinstance(aspect_ratio, tuple):
+        image["aspectRatio"] = {"width": aspect_ratio[0], "height": aspect_ratio[1]}
+
+    return image
