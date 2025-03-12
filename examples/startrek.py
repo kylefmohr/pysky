367b238698505df026b042a7b843a8384ac15439
Todd Federman <tfederman@hotmail.com>
2025-03-11 23:53:45 -0400

    formatting

--- examples/startrek.py
+++ examples/startrek.py
@@ -11,7 +11,10 @@ bsky = BskyClient()
 
 con = psycopg2.connect(cursor_factory=psycopg2.extras.NamedTupleCursor)
 cursor = con.cursor()
-cursor.execute("select * from trek_episodes where month=%s and day=%s order by date,season desc limit 1", (datetime.now().month, datetime.now().day))
+cursor.execute(
+    "select * from trek_episodes where month=%s and day=%s limit 1",
+    (datetime.now().month, datetime.now().day),
+)
 
 row = cursor.fetchone()
 
@@ -19,16 +22,19 @@ if not row:
     sys.stderr.write("No episodes aired on this day")
     exit(0)
 
-markdown_text = f"""
+post_text_markdown = f"""
 {row.date.strftime("%B %d, %Y").replace(" 0", " ")}
 ["{row.title}"]({row.link})
 {row.show} - Season {row.season}, Episode {row.episode}
 """.strip()
 
-images = sorted([f"images/{f}" for f in os.listdir("images") if f"{slugify(row.show)}-season-{row.season}-episode-{row.episode}-" in f])
+images = sorted(f"images/{f}" for f in os.listdir("images"))
+images = [
+    im for im in images if f"{slugify(row.show)}-season-{row.season}-episode-{row.episode}-" in im
+]
 
 alt_texts = [
-    f'Episode summary: {row.description}',
+    f"Episode summary: {row.description}",
     f'Image from "{row.title}", episode {row.episode} of season {row.season} of {row.show}',
     f'Image from "{row.title}", episode {row.episode} of season {row.season} of {row.show}',
     f'Image from "{row.title}", episode {row.episode} of season {row.season} of {row.show}',
@@ -39,15 +45,19 @@ unique_key_reply = f"startrek-bot:{row.show}:{row.season}:{row.episode}:2"
 
 blob_uploads_post = [bsky.upload_image(image_path=i) for i in images[0:4]]
 
-bsky.create_post(markdown_text=markdown_text,
-                 blob_uploads=blob_uploads_post,
-                 alt_texts=alt_texts,
-                 client_unique_key=unique_key_post)
+bsky.create_post(
+    markdown_text=post_text_markdown,
+    blob_uploads=blob_uploads_post,
+    alt_texts=alt_texts,
+    client_unique_key=unique_key_post,
+)
 
 blob_uploads_reply = [bsky.upload_image(image_path=i) for i in images[4:8]]
 
-bsky.create_post(text="",
-                 blob_uploads=blob_uploads_reply,
-                 alt_texts=alt_texts,
-                 client_unique_key=unique_key_reply,
-                 reply_client_unique_key=unique_key_post)
+bsky.create_post(
+    text="",
+    blob_uploads=blob_uploads_reply,
+    alt_texts=alt_texts,
+    client_unique_key=unique_key_reply,
+    reply_client_unique_key=unique_key_post,
+)
