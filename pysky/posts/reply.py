9a2f2040aaddec64e61c791fd99207e0f455950b
Todd Federman <tfederman@hotmail.com>
2025-03-13 13:06:05 -0400

    move reply ref assembly code

--- pysky/posts/reply.py
+++ pysky/posts/reply.py
@@ -1,4 +1,6 @@
-# add db lookup from client unique key
+import re
+
+from pysky.models import BskyPost, APICallLog
 
 class Reply:
 
@@ -21,3 +23,32 @@ class Reply:
                 "parent": {"cid": post.cid, "uri": post.uri},
                 "root": {"cid": post.cid, "uri": post.uri},
             }
+
+
+    @staticmethod
+    def from_uri(uri):
+        pattern_1 = "at://([^/]+)/([^/]+)/([a-z0-9]+)"
+        pattern_2 = "https://bsky.app/profile/([^/]+)/(post)/([a-z0-9]+)"
+        m = re.match(pattern_1, uri) or re.match(pattern_2, uri)
+        assert m, f"invalid reply_uri: {uri}"
+        reply_repo, collection, reply_rkey = m.groups()
+        assert collection in [
+            "app.bsky.feed.post",
+            "post",
+        ], f"invalid collection for reply: {collection}"
+        return Reply(reply_repo, reply_rkey)
+
+
+    @staticmethod
+    def from_client_unique_key(did, client_unique_key):
+        parent = (
+            BskyPost.select(BskyPost.uri)
+            .join(APICallLog)
+            .where(
+                BskyPost.client_unique_key == client_unique_key,
+                APICallLog.request_did == did,
+            )
+            .first()
+        )
+        assert parent, "can't create a reply to an invalid parent"
+        return Reply.from_uri(parent.uri)
