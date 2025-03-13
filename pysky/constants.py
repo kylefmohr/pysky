d1cddaeff43739f3c3a1ab63c0d720c0d9bdae48
Todd Federman <tfederman@hotmail.com>
2025-03-12 23:02:34 -0400

    remove video related constant

--- pysky/constants.py
+++ pysky/constants.py
@@ -6,12 +6,3 @@ HOSTNAME_VIDEO = "video.bsky.app"
 AUTH_METHOD_PASSWORD, AUTH_METHOD_TOKEN = range(2)
 
 ALLOWED_VIDEO_MIME_TYPES = ["video/mp4", "video/mpeg", "video/webm", "video/quicktime", "image/gif"]
-
-INTERMEDIATE_VIDEO_PROCESSING_STATES = [
-    "JOB_STATE_CREATED",
-    "JOB_STATE_ENCODING",
-    "JOB_STATE_SCANNING",
-    "JOB_STATE_SCANNED",
-    "JOB_STATE_UPLOADED",
-    "JOB_STATE_COMPLETED",
-]
