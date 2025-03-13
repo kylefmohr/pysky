5ff99179d7d597b7465b68d29069fabb8b439ed2
Todd Federman <tfederman@hotmail.com>
2025-03-12 23:22:12 -0400

    add video to post helpers

--- pysky/helpers.py
+++ pysky/helpers.py
@@ -4,7 +4,7 @@ from itertools import zip_longest
 import bs4
 import markdown
 
-def get_post(text, blob_uploads=None, alt_texts=None, facets=None, reply=None, markdown_text=None):
+def get_post(text, blob_uploads=None, alt_texts=None, facets=None, reply=None, markdown_text=None, video_blob_upload=None):
 
     if markdown_text:
         post = get_post_from_markdown(markdown_text)
@@ -24,8 +24,28 @@ def get_post(text, blob_uploads=None, alt_texts=None, facets=None, reply=None, m
     if blob_uploads:
         post["embed"] = get_image_embed(blob_uploads, alt_texts)
 
+    if video_blob_upload:
+        post["embed"] = get_video_embed(video_blob_upload)
+
     return post
 
+
+def get_video_embed(blob_upload):
+
+    aspect_ratio = blob_upload.blob.aspect_ratio
+
+    return {
+        "$type": "app.bsky.embed.video",
+        "aspectRatio": {"width": aspect_ratio[0], "height": aspect_ratio[1]},
+        "video": {
+            "$type": "blob",
+            "ref": {
+                "$link": getattr(blob_upload.blob.ref, "$link")
+            },
+            "mimeType": blob_upload.blob.mimeType,
+            "size": blob_upload.blob.size,
+        },
+    }
 
 def get_image_embed(blob_uploads, alt_texts):
     return {
@@ -50,7 +70,7 @@ def get_image(blob_upload, alt_text=None):
         },
     }
 
-    aspect_ratio = getattr(blob_upload, "aspect_ratio", None)
+    aspect_ratio = getattr(blob_upload.blob, "aspect_ratio", None)
     if isinstance(aspect_ratio, tuple):
         image["aspectRatio"] = {"width": aspect_ratio[0], "height": aspect_ratio[1]}
 
