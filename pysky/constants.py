2b7fc8bee6e72eac1e57987b0d44bba3a51acb65
Todd Federman <tfederman@hotmail.com>
2025-03-12 23:02:18 -0400

    add some video related constants

--- pysky/constants.py
+++ pysky/constants.py
@@ -4,3 +4,14 @@ HOSTNAME_CHAT = "api.bsky.chat"
 HOSTNAME_VIDEO = "video.bsky.app"
 
 AUTH_METHOD_PASSWORD, AUTH_METHOD_TOKEN = range(2)
+
+ALLOWED_VIDEO_MIME_TYPES = ["video/mp4", "video/mpeg", "video/webm", "video/quicktime", "image/gif"]
+
+INTERMEDIATE_VIDEO_PROCESSING_STATES = [
+    "JOB_STATE_CREATED",
+    "JOB_STATE_ENCODING",
+    "JOB_STATE_SCANNING",
+    "JOB_STATE_SCANNED",
+    "JOB_STATE_UPLOADED",
+    "JOB_STATE_COMPLETED",
+]
