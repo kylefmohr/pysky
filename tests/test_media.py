8954b0de1a3724f95bda0b24b13f167face45567
Todd Federman <tfederman@hotmail.com>
2025-03-16 23:46:42 -0400

    fix tests for new aspect ratio format

--- tests/test_media.py
+++ tests/test_media.py
@@ -28,18 +28,18 @@ def test_image_aspect_ratio():
     from pysky import Image
 
     expected_ar = [
-        ("image1.gif", (475, 357)),
-        ("image2.gif", (480, 360)),
-        ("image3.gif", (400, 225)),
-        ("image1.jpg", (640, 853)),
-        ("image2.jpg", (487, 261)),
-        ("image3.jpg", (1440, 2560)),
-        ("image1.png", (880, 641)),
-        ("image2.png", (640, 339)),
-        ("image3.png", (756, 932)),
-        ("image1.webp", (973, 1458)),
-        ("image2.webp", (460, 801)),
-        ("image3.webp", (500, 500)),
+        ("image1.gif", {"width": 475, "height": 357}),
+        ("image2.gif", {"width": 480, "height": 360}),
+        ("image3.gif", {"width": 400, "height": 225}),
+        ("image1.jpg", {"width": 640, "height": 853}),
+        ("image2.jpg", {"width": 487, "height": 261}),
+        ("image3.jpg", {"width": 1440, "height": 2560}),
+        ("image1.png", {"width": 880, "height": 641}),
+        ("image2.png", {"width": 640, "height": 339}),
+        ("image3.png", {"width": 756, "height": 932}),
+        ("image1.webp", {"width": 973, "height": 1458}),
+        ("image2.webp", {"width": 460, "height": 801}),
+        ("image3.webp", {"width": 500, "height": 500}),
     ]
 
     for img, ar in expected_ar:
@@ -52,10 +52,10 @@ def test_video_aspect_ratio():
     from pysky import Video
 
     expected_ar = [
-        ("video1.mp4", (640, 360)),
-        ("video2.mp4", (720, 720)),
-        ("video3.mp4", (480, 600)),
-        ("video1.mov", (852, 480)),
+        ("video1.mp4", {"width": 640, "height": 360}),
+        ("video2.mp4", {"width": 720, "height": 720}),
+        ("video3.mp4", {"width": 480, "height": 600}),
+        ("video1.mov", {"width": 852, "height": 480}),
     ]
 
     for vid, ar in expected_ar:
