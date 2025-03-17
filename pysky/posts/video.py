02b09c33a904b7dc958159279c8afbb4a7f249d2
Todd Federman <tfederman@hotmail.com>
2025-03-16 23:07:01 -0400

    revert video upload back to app.bsky.video.uploadVideo

--- pysky/posts/video.py
+++ pysky/posts/video.py
@@ -1,11 +1,19 @@
 import os
+import time
+import json
+from itertools import count
+from types import SimpleNamespace
 
 import ffmpeg
 
 from pysky.logging import log
+from pysky.exceptions import ExcessiveIteration
 from pysky.mimetype import guess_file_type
+from pysky.exceptions import APIError
+
 
 ALLOWED_STREAM_BRAND_PREFIXES = ["mp4","qt","iso5","iso6","isom"]
+
 
 class IncompatibleMedia(Exception):
     pass
@@ -22,7 +30,6 @@ class Video:
             raise IncompatibleMedia(f"The file \"{self.filename}\" doesn't appear to be a format that's compatible with Bluesky")
 
 
-    # to do - handle 409 already_exists - Video already processed
     def upload(self, bsky):
 
         if not self.mimetype:
@@ -35,13 +42,56 @@ class Video:
 
         data = open(self.filename, "rb").read()
 
-        self.upload_response = bsky.upload_blob(data, self.mimetype)
-        print(self.upload_response)
+        params = {"did": bsky.did, "name": self.filename.split("/")[-1]}
 
         try:
-            self.aspect_ratio = self.get_aspect_ratio()
+            uploaded_blob = bsky.post(
+                params=params,
+                data=data,
+                endpoint="xrpc/app.bsky.video.uploadVideo",
+                headers={"Content-Type": self.mimetype},
+            )
+        except APIError as e:
+            # 409 means: "error":"already_exists", "message":"Video already processed"
+            if e.apilog.http_status_code == 409:
+                log.info(f'video "{self.filename}" already uploaded and processed')
+                response_json = json.loads(e.apilog.exception_response)
+                uploaded_blob = SimpleNamespace(jobId=response_json["jobId"])
+            else:
+                raise
+
+        processed_blob = None
+
+        for n in count():
+            r = bsky.get(endpoint="xrpc/app.bsky.video.getJobStatus", jobId=uploaded_blob.jobId)
+
+            if r.jobStatus.state == "JOB_STATE_COMPLETED":
+                processed_blob = r.jobStatus.blob
+                break
+            elif r.jobStatus.state == "JOB_STATE_FAILED":
+                raise Exception(
+                    f"error state in video processing: {r.jobStatus.state} (jobId {uploaded_blob.jobId})"
+                )
+            elif n > 500:
+                raise ExcessiveIteration(
+                    f"waited too long for video upload processing (jobId {uploaded_blob.jobId})"
+                )
+
+            time.sleep(2)
+
+        # only relevant after the processing is finished
+        try:
+            if processed_blob:
+                self.aspect_ratio = self.get_aspect_ratio()
         except Exception as e:
-            log.warning(f"error finding image aspect ratio")
+            log.error(f"can't get aspect ratio for {self.filename}: {e}")
+
+        if processed_blob:
+            # push the blob struct one level down for
+            # consistency with the image upload response blob
+            self.upload_response = SimpleNamespace(blob=processed_blob)
+        else:
+            self.upload_response = uploaded_blob
 
         return self.upload_response
 
@@ -51,7 +101,6 @@ class Video:
         assert self.upload_response and hasattr(
             self.upload_response, "blob"
         ), f"video {self.filename} hasn't been successfully uploaded yet"
-        # assert aspect_ratio?
 
         video = {
             "$type": "app.bsky.embed.video",
